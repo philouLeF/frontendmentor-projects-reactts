cbf89fae36436b76cbf720d623b7be6d4cf11c52
philouLeF <delcroixphilippe73@gmail.com>
2023-07-29 17:50:03 +0200

    init

---
+++ src/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="shadow-lg px-5 py-5">
+      <h1 className="text-xl">Frontend Mentor Projects</h1>
+    </header>
+  );
+};
+
+export default Header;
